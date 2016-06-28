71fa6e61d7e08899e456f095204f2382b6335bfe
RoobScoobs <iruben229@gmail.com>
2016-06-28 09:28:47 -0400

    Update view all runs
    Add table on the front-end with dynamic data

--- assets/scripts/runs/runs-ui.js
+++ assets/scripts/runs/runs-ui.js
@@ -6,9 +6,13 @@ const failure = function (error) {
   console.error(error);
 };
 
+
 const getRunsSuccess = function (data) {
-  $('.display-runs').text(data.runs);
-  console.log(data.runs);
+  let anything = data.runs;
+  $('.display-runs').html("<table class='table'><tr id='row1'><th>Description</th><th>Total Miles</th><th>Total Time</th><th>Run Completion Date</th></tr></table>");
+  for (let i = 0; i < anything.length; i++) {
+    $('#row1').after("<tr><td>" + anything[i]['description'] + "</td><td>"+anything[i]['total_miles']+"</td><td>"+anything[i]['total_time']+"</td><td>"+anything[i]['completion_date']+"</td></tr>");
+  };
 };
 
 const createRunsSuccess = function (data) {
