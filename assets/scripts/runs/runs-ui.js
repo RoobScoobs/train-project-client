5d3b94aaeb7966c212061467a20a2fbb58256a96
RoobScoobs <iruben229@gmail.com>
2016-06-28 13:48:45 -0400

    Update Runs UI
    Make delete button visible

--- assets/scripts/runs/runs-ui.js
+++ assets/scripts/runs/runs-ui.js
@@ -10,12 +10,12 @@ const failure = function (error) {
 
 const getRunsSuccess = function (data) {
   let viewRuns = data.runs;
-  $('.display-runs').html("<table class='table table-hover'><tr id='row1'><th>Description</th><th>Total Miles</th><th>Total Time</th><th>Run Completion Date</th><th> </th</tr></table>");
+  $('.display-runs').html("<table class='table table-hover'><tr id='row1'><th>Description</th><th>Total Miles</th><th>Total Time</th><th>Date</th><th> </th</tr></table>");
   for (let i = 0; i < viewRuns.length; i++) {
     $('#row1').after("<tr><td>" + viewRuns[i]['description'] +
     "</td><td>"+viewRuns[i]['total_miles'] + "</td><td>" + viewRuns[i]['total_time'] +
     "</td><td>"+viewRuns[i]['completion_date'] + "</td><td>" +
-    "<button type='button' data-id=" + viewRuns[i].id + " class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+    "<button type='button' data-id=" + viewRuns[i].id + " class='close' aria-label='Close'><span class='x' aria-hidden='true'>&times;</span></button>" +
     "</td></tr>");
   };
 };
